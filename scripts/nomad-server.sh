0182466eb388265b959eb80044d290f885ae4ebc
Ron Kurr <kurr@kurron.org>
2016-02-27 21:51:04 +0000

    Added Nomad server

---
+++ scripts/nomad-server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CMD="docker run \
+       --restart=always \
+       --net "host" \
+       --volume /var/run/docker.sock:/var/run/docker.sock \
+       kurron/docker-nomad:latest gent -bind=10.10.10.10 -dc=vagrant -region=USA -server -bootstrap-expect=1"
+
+echo $CMD
+eval $CMD $*
