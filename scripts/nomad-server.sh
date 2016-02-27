2d2a2f6741d0305289e1ee1fb6c121fc5361bdbd
Ron Kurr <kurr@kurron.org>
2016-02-27 22:20:10 +0000

    Added a container name

--- scripts/nomad-server.sh
+++ scripts/nomad-server.sh
@@ -4,6 +4,7 @@ CMD="docker run \
        --detach \
        --restart=always \
        --net "host" \
+       --name nomad-server \
        --volume /var/run/docker.sock:/var/run/docker.sock \
        kurron/docker-nomad:latest agent -bind=10.10.10.10 -data-dir=/var/lib/nomad -dc=vagrant -region=USA -server -bootstrap-expect=1"
 
