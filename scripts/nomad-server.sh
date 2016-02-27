f915ddd44b0d685cc5b0339026ecd30bf80e6a75
Ron Kurr <kurr@kurron.org>
2016-02-27 22:07:20 +0000

    Added data directory

--- scripts/nomad-server.sh
+++ scripts/nomad-server.sh
@@ -4,7 +4,7 @@ CMD="docker run \
        --restart=always \
        --net "host" \
        --volume /var/run/docker.sock:/var/run/docker.sock \
-       kurron/docker-nomad:latest agent -bind=10.10.10.10 -dc=vagrant -region=USA -server -bootstrap-expect=1"
+       kurron/docker-nomad:latest agent -bind=10.10.10.10 -data-dir=/var/lib/nomad -dc=vagrant -region=USA -server -bootstrap-expect=1"
 
 echo $CMD
 eval $CMD $*
