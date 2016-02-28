eca04975f5fbed368e63d91c776c9be38b760e0f
Ron Kurr <kurr@kurron.org>
2016-02-28 16:41:47 +0000

    Added Charlies support

---
+++ scripts/charlie.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CMD="docker run \
+       --detach \
+       --restart=always \
+       --net "host" \
+       --name nomad-client \
+       --volume /var/run/docker.sock:/var/run/docker.sock \
+       kurron/docker-nomad:latest agent -bind=10.10.10.30\
+                                        -data-dir=/var/lib/nomad \
+                                        -dc=vagrant \
+                                        -region=USA \
+                                        -client \
+                                        -servers 10.10.10.10 \
+                                        -node-class=general-purpose"
+
+echo $CMD
+eval $CMD $*
