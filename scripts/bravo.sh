e803a7a4eb1f32f04f12590388ce391d0a1b769f
Ron Kurr <kurr@kurron.org>
2016-02-28 16:23:11 +0000

    Fixed typo

--- scripts/bravo.sh
+++ scripts/bravo.sh
@@ -12,7 +12,7 @@ CMD="docker run \
                                         -region=USA \
                                         -client \
                                         -servers 10.10.10.10:4646 \
-                                        -node-id bravo \
+                                        -node-id=bravo \
                                         -node-class=general-purpose"
 
 echo $CMD
